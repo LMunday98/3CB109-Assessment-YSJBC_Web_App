c1f164b18a358fdb296a7fa0aea29bbb758b2a67
LMunday98 <luke.munday@gmail.com>
2021-02-25 21:44:24 +0000

    Added mvc structure - added routes to controllers

--- app/views.py
+++ app/views.py
@@ -4,6 +4,9 @@ from flask import render_template
 
 from app import app
 
+# Load the controllers
+from app.controllers import *
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -12,3 +15,5 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+app.route('/hello')(hello.hello)
