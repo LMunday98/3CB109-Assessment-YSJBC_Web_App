ccf10bbed60df69e9a66804cfaa801e11265e498
LMunday98 <luke.munday@gmail.com>
2021-02-25 23:22:45 +0000

    Added MVC to new refactor

--- project/app/views.py
+++ project/app/views.py
@@ -4,6 +4,8 @@ from flask import render_template
 
 from app import app
 
+from app.controllers import *
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -12,3 +14,5 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+app.route('/hello')(hello.hi)
