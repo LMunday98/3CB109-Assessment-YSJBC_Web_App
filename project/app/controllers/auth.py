db1234e7787dc1a878d82d1633af0b8c91dc37f7
LMunday98 <luke.munday@gmail.com>
2021-02-26 01:00:11 +0000

    refactored insert into db class

--- project/app/controllers/auth.py
+++ project/app/controllers/auth.py
@@ -1,13 +1,16 @@
-from app.db import connection
+from app.db import insert
 from flask import render_template
 
 def login(form_data):
     name = form_data['name']
     password = form_data['password']
 
-    cursor, conn = connection()
-    cursor.execute("insert into users (user_name,user_password) values(%s,%s)", (name,password))
-    conn.commit()
-    cursor.close()
+    table = "users"
+    fields = "user_name,user_password"
+    data_types = "%s,%s"
+    content = [name, password]
+
+    data = [table, fields, data_types, content]
+    insert(data)
 
     return render_template("index.html")
--- project/app/db.py
+++ project/app/db.py
@@ -1,10 +1,17 @@
 import MySQLdb
 
 def connection():
-    conn = MySQLdb.connect(host="127.0.0.1",
-                           user = "root",
-                           passwd = "",
-                           db = "flask")
+    conn = MySQLdb.connect(
+        host="127.0.0.1",
+        user = "root",
+        passwd = "",
+        db = "flask")
     c = conn.cursor()
 
     return c, conn
+
+def insert(data):
+    cursor, conn = connection()
+    cursor.execute("insert into " + data[0] + " (" + data[1] + ") values(" + data[2] + ")", (data[3]))
+    conn.commit()
+    cursor.close()
