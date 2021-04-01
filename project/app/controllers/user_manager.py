59aec74bb21ed64d1febbcf5412e456b37fb861e
luke.munday <luke.munday@gmail.com>
2021-04-01 16:31:21 +0100

    Gets all users if user id not given

--- project/app/controllers/user_manager.py
+++ project/app/controllers/user_manager.py
@@ -1,5 +1,12 @@
 from app.models.user import *
 from flask import render_template, redirect
 
-def get(id):
-    return render_template('admin/user_manager.html', user_id=id)
+def get(user_id):
+    if user_id == None:
+        # Get all users
+        users = User.query.all()
+        return render_template('admin/user_manager.html', users=users)
+    else:
+        # Get specific user
+        x=1
+    return render_template('admin/user_manager.html', user_id=user_id)
