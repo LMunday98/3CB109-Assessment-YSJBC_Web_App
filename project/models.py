babdf49b6a4c42ddfb2d7959193dd28d2844f71b
luke.munday <luke.munday@gmail.com>
2021-04-15 22:35:17 +0100

    Update event seeder to new model format

--- project/models.py
+++ project/models.py
@@ -1,7 +1,7 @@
 from app import db
 from app.models import *
 from faker import Faker
-from datetime import datetime
+import datetime
 
 def migrate():
     print ("Migrating database...")
@@ -27,23 +27,22 @@ def seed():
         for _ in range(12):
             Blog.seed(fake,_+1)
         # Monday
-        Event.create("Water Session", "event-1")
-        #Event.create("Water Session", "event-1", datetime(2021, 4, 12, 9, 30), datetime(2021, 4, 12, 10, 30))
-        #Event.create("Erg", "event-2", datetime(2021, 4, 12, 11, 30), datetime(2021, 4, 12, 13, 45))
-        #Event.create("Yoga", "event-4", datetime(2021, 4, 12, 14, 00), datetime(2021, 4, 12, 15, 00))
+        Event.create("Water Session", "event-1", datetime.date(2021, 4, 12), datetime.time(9, 30), datetime.time(10, 30))
+        Event.create("Erg", "event-2", datetime.date(2021, 4, 12), datetime.time(11, 30), datetime.time(13, 45))
+        Event.create("Yoga", "event-4", datetime.date(2021, 4, 12), datetime.time(14, 00), datetime.time(15, 00))
         # Tuesday
-        #Event.create("Weight Training", "event-3", datetime(2021, 4, 13, 12, 30), datetime(2021, 4, 13, 14, 0))
-        #Event.create("Yoga", "event-4", datetime(2021, 4, 13, 14, 30), datetime(2021, 4, 13, 15, 30))
+        Event.create("Weight Training", "event-3", datetime.date(2021, 4, 13),  datetime.time(12, 30), datetime.time(14, 0))
+        Event.create("Yoga", "event-4", datetime.date(2021, 4, 13),  datetime.time(14, 30), datetime.time(15, 30))
         # Wednesday
-        #Event.create("Water Session", "event-1", datetime(2021, 4, 14, 9, 30), datetime(2021, 4, 14, 12, 30))
-        #Event.create("Water Session", "event-1", datetime(2021, 4, 14, 13, 30), datetime(2021, 4, 14, 16, 30))
+        Event.create("Water Session", "event-1", datetime.date(2021, 4, 14),  datetime.time(9, 30), datetime.time(12, 30))
+        Event.create("Water Session", "event-1", datetime.date(2021, 4, 14),  datetime.time(13, 30), datetime.time(16, 30))
         # Thursday
-        #Event.create("Erg", "event-2", datetime(2021, 4, 15, 15, 30), datetime(2021, 4, 15, 16, 30))
-        #Event.create("Yoga", "event-4", datetime(2021, 4, 15, 17, 30), datetime(2021, 4, 15, 18, 30))
+        Event.create("Erg", "event-2", datetime.date(2021, 4, 15),  datetime.time(15, 30), datetime.time(16, 30))
+        Event.create("Yoga", "event-4", datetime.date(2021, 4, 15),  datetime.time(17, 30), datetime.time(18, 30))
         # Friday
-        #Event.create("Water Session", "event-1", datetime(2021, 4, 16, 9, 30), datetime(2021, 4, 16, 10, 30))
+        Event.create("Water Session", "event-1", datetime.date(2021, 4, 16),  datetime.time(9, 30), datetime.time(10, 30))
         # Monday
-        #Event.create("Water Session", "event-1", datetime(2021, 4, 19, 9, 30), datetime(2021, 4, 19, 10, 30))
+        Event.create("Water Session", "event-1", datetime.date(2021, 4, 19),  datetime.time(9, 30), datetime.time(10, 30))
         return "Done!"
     except Exception as e:
         return(str(e))
