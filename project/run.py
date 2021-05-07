ed456419b7cd3cacb32bbfcaeca5e7fd732cff24
luke.munday <luke.munday@gmail.com>
2021-05-07 17:58:19 +0100

    Change main menu buttons

--- project/run.py
+++ project/run.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     # to prevent circular dependencies
     from app.routes import *
     
-    app.run(host='192.168.0.184', port=5000)
+    app.run(host='0.0.0.0', port=5000)
     #app.run(host='cs2s.yorkdc.net', port=5018)
